ec9d75fd8e50b9ebdca2e4cb3ee8204ec27c0e46
Ian Wright <ian@authentise.com>
2017-09-16 14:01:50 -0600

    Create icon components
    Icon components can take a width prop. We can enhance these components
    for more customization if needed.

---
+++ src/components/icons/FiltersIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FiltersIcon = ({width}) => (
+  <svg id="c0ec20f0-9e48-4c81-bf8a-14d6ccddba61" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width={width}><title>icon-filters</title><line x1="28.73" y1="19.43" x2="14.56" y2="19.43" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.82"/><circle cx="30.86" cy="19.43" r="2.13" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.82"/><line x1="22.76" y1="25" x2="35.44" y2="25" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.82"/><circle cx="20.63" cy="25" r="2.13" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.82"/><line x1="14.56" y1="30.58" x2="26.6" y2="30.58" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.82"/><circle cx="28.73" cy="30.58" r="2.13" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.82"/><line x1="32.99" y1="19.43" x2="35.44" y2="19.43" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.82"/><line x1="18.5" y1="25" x2="14.56" y2="25" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.82"/><line x1="30.86" y1="30.58" x2="35.44" y2="30.58" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.82"/></svg>
+);
+
+FiltersIcon.defaultProps = { width: '100%' }
+FiltersIcon.propTypes = { width: PropTypes.string }
+
+export default FiltersIcon;
