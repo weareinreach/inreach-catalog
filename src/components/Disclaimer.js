4486bbcb9ebe97003af995736901900e09afdb9e
Ian Wright <ian@authentise.com>
2017-09-28 21:33:52 -0600

    Create Disclaimer component and add to Announcement

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Dialog, { DialogTitle } from 'material-ui/Dialog';
 import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
 
-const Disclaimer = () => (
-  <p>
-    disclaimer
-  </p>
+import AsylumConnectButton from './AsylumConnectButton';
+
+const styles = (theme) => ({});
+
+const Disclaimer = ({handleRequestClose, isOpen}) => (
+  <Dialog onRequestClose={handleRequestClose} open={isOpen}>
+    <DialogTitle>
+      Disclaimer
+    </DialogTitle>
+    <div>
+      I'm a disclaimer
+    </div>
+    <AsylumConnectButton
+      onClick={handleRequestClose}
+      variant="secondary"
+    >
+      OK
+    </AsylumConnectButton>
+  </Dialog>
 );
 
-export default Disclaimer;
+Disclaimer.propTypes = {
+  handleRequestClose: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(Disclaimer);
