3b362944191eff11db47d8fec8f7a8ae1261adbb
Ian Wright <ian@authentise.com>
2017-09-29 14:31:47 -0600

    Add Disclaimer content and styles

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -7,22 +7,31 @@ import { withStyles } from 'material-ui/styles';
 
 import AsylumConnectButton from './AsylumConnectButton';
 
-const styles = (theme) => ({});
+const styles = (theme) => ({
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '4rem',
+  },
+  disclaimerBody: { padding: '5.5rem' },
+  title: { paddingBottom: '1rem', textAlign: 'center', },
+});
 
-const Disclaimer = ({handleRequestClose, isOpen}) => (
-  <Dialog onRequestClose={handleRequestClose} open={isOpen}>
-    <DialogTitle>
-      Disclaimer
-    </DialogTitle>
-    <div>
-      I'm a disclaimer
+const Disclaimer = ({classes, handleRequestClose, isOpen}) => (
+  <Dialog open={isOpen} onRequestClose={handleRequestClose}>
+    <div className={classes.disclaimerBody}>
+      <Typography className={classes.title} type="display1">
+        AsylumConnect Disclaimer
+      </Typography>
+      <Typography type="body1">
+        The AsylumConnect team will do its best to confirm basic facts about service providers listed on this website. However, we lack the necessary resources to fully vet service providers and make no representations regarding the viability or capabilities of any such providers. Consequently, AsylumConnect assumes no responsibility for the actions of providers listed on this website and asylum seekers who contact any such providers do so at their own risk.
+      </Typography>
+      <div className={classes.buttonContainer}>
+        <AsylumConnectButton onClick={handleRequestClose} variant="secondary">
+          OK
+        </AsylumConnectButton>
+      </div>
     </div>
-    <AsylumConnectButton
-      onClick={handleRequestClose}
-      variant="secondary"
-    >
-      OK
-    </AsylumConnectButton>
   </Dialog>
 );
 
