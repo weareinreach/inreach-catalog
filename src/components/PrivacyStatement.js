4aac329a958750d9faa710fbaa1a3b917606b999
Ian Wright <ian@authentise.com>
2017-09-29 16:24:53 -0600

    Add PrivacyStatement content and styles

--- src/components/PrivacyStatement.js
+++ src/components/PrivacyStatement.js
@@ -8,17 +8,30 @@ import { withStyles } from 'material-ui/styles';
 import AsylumConnectButton from './AsylumConnectButton';
 
 const styles = (theme) => ({
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '4rem',
+  },
+  disclaimerBody: { padding: '5.5rem' },
+  paperWidthSm: { maxWidth: '650px' },
+  title: { paddingBottom: '1rem', textAlign: 'center', },
 });
 
 const PrivacyStatement = ({classes, handleRequestClose, isOpen}) => (
-  <Dialog open={isOpen} onRequestClose={handleRequestClose}>
-    <div>
-      <Typography type="display1">
-        Privacy Statement
+  <Dialog
+    classes={{ paperWidthSm: classes.paperWidthSm }}
+    open={isOpen}
+    onRequestClose={handleRequestClose}
+  >
+    <div className={classes.disclaimerBody}>
+      <Typography className={classes.title} type="display1">
+        AsylumConnect User Privacy Statement
       </Typography>
       <Typography type="body1">
+        The AsylumConnect catalog uses Google Analytics with anonymized IP addresses to help analyze how visitors use this site. Google Analytics uses cookies, which are small text files placed on your computer, to collect standard visitor behavior information in an anonymous form. No personally identifiable information is collected about you, unless you explicitly submit that information on this website. If you would like to opt-out of Google Analytics, you may do so by clicking here.
       </Typography>
-      <div>
+      <div className={classes.buttonContainer}>
         <AsylumConnectButton onClick={handleRequestClose} variant="secondary">
           OK
         </AsylumConnectButton>
