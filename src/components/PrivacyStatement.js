73f4663c52e9361226ad6bd8bdf4263c232d2b6b
Ian Wright <ian@authentise.com>
2017-09-30 15:04:02 -0600

    Add PrivacyStatement links and global link color

--- src/components/PrivacyStatement.js
+++ src/components/PrivacyStatement.js
@@ -29,7 +29,7 @@ const PrivacyStatement = ({classes, handleRequestClose, isOpen}) => (
         AsylumConnect User Privacy Statement
       </Typography>
       <Typography type="body1">
-        The AsylumConnect catalog uses Google Analytics with anonymized IP addresses to help analyze how visitors use this site. Google Analytics uses cookies, which are small text files placed on your computer, to collect standard visitor behavior information in an anonymous form. No personally identifiable information is collected about you, unless you explicitly submit that information on this website. If you would like to opt-out of Google Analytics, you may do so by clicking here.
+        The AsylumConnect catalog uses Google Analytics with <a href="https://support.google.com/analytics/answer/2763052?hl=en" target="_blank">anonymized IP addresses</a> to help analyze how visitors use this site. Google Analytics uses cookies, which are small text files placed on your computer, to collect standard visitor behavior information in an anonymous form. No personally identifiable information is collected about you, unless you explicitly submit that information on this website. If you would like to opt-out of Google Analytics, you may do so by clicking <a href="https://tools.google.com/dlpage/gaoptout" target="_blank">here</a>.
       </Typography>
       <div className={classes.buttonContainer}>
         <AsylumConnectButton onClick={handleRequestClose} variant="secondary">
