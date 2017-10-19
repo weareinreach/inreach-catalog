d27ae1c5b6eb70a42093cbcf4ebb9cadedf8eaa3
Thuy <tpham045@hotmail.com>
2017-10-19 00:49:28 +0000

    style: add `power by Google Translation` text
    Users can view the text when clicking on Language button
    The `Powered By Google Translation` is displayed as a sticker at the top of the list

--- src/components/Language.js
+++ src/components/Language.js
@@ -2,11 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import langs from 'langs';
 import ValidLanguageList from '../helpers/ValidLanguageList';
-
+import List, {ListItem, ListItemText, ListSubheader} from 'material-ui/List';
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
-import Menu, { MenuItem } from 'material-ui/Menu';
-import IconButton from 'material-ui/IconButton';
 import Typography from 'material-ui/Typography';
 
 import ChevronIcon from './icons/ChevronIcon';
@@ -15,19 +13,44 @@ const styles = theme => ({
   root: {
     display: 'block'
   },
+  languageList: {
+    position: 'absolute',
+    zIndex: 3,
+    paddingTop: 0,
+    background: theme.palette.background.paper,
+    overflow: 'auto',
+    maxHeight: 300,
+    borderRadius: '2px',
+    boxShadow: theme.shadows[9]
+  },
+  poweredByGoogle: {
+    display: 'flex',
+    fontFamily: 'arial',
+    fontSize: '11px',
+    color: '#666',
+    whiteSpace: 'nowrap',
+  },
+  gooLogoLink: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  gooLogoImg: {
+    paddingRight: '4px',
+    paddingLeft: '4px'
+  },
+  blackTranslateColor: {
+    display: 'inline',
+    fontSize: '12px',
+    color: '#444',
+    fontWeight: 'bold',
+    textDecoration: 'none',
+  },
   lowercaseText: {
     textTransform: 'capitalize'
   },
   centerTextAlign: {
     textAlign: 'center',
-  },
-  AsylumConnectMenu: {
-    marginTop: '56px'
-  },
-  hiddenTranslator: {
-    visibility: 'hidden',
-    width: '0',
-    height: '0'
   }
 });
 
@@ -42,7 +65,9 @@ class LangMenuItem extends React.Component {
   }
   render() {
     return (
-      <MenuItem onClick={this.handleSelectLang} children={this.props.langName}></MenuItem>
+      <ListItem button onClick={this.handleSelectLang}>
+        <ListItemText primary={this.props.langName} />
+      </ListItem>
     )
   }
 }
@@ -51,15 +76,14 @@ class Language extends React.Component {
   constructor() {
     super();
     this.state = {
-      anchorEl: null,
-      open: false,
+      open: false
     }
     this.handleClick = this.handleClick.bind(this)
     this.handleRequestClose = this.handleRequestClose.bind(this)
     this.handleRequestCloseAfterSelect = this.handleRequestCloseAfterSelect.bind(this)
   }
   handleClick(event) {
-    this.setState({ open: true, anchorEl: event.currentTarget });
+    this.setState({ open: !this.state.open });
   };
   
   handleRequestClose() {
@@ -87,20 +111,21 @@ class Language extends React.Component {
           </Typography>
           <ChevronIcon width={'20px'}/>
         </Button>
-        <Menu 
-          id="simple-menu"
-          anchorEl={this.state.anchorEl}
-          open={this.state.open}
-          onRequestClose={this.handleRequestClose}
-          className={classes.AsylumConnectMenu}>
-            {
-              langsList.map(function(lang, index) {
-                return (
-                  <LangMenuItem key={index} langName={lang.name} langCode={lang['1']} handleSelectLang={this.handleRequestCloseAfterSelect} />
-                )
-              }, this)
-            }
-        </Menu>
+        {this.state.open &&
+          <List className={classes.languageList}>
+            <ListSubheader className={classes.poweredByGoogle}>
+              <span>Powered By</span>
+              <a className={classes.gooLogoLink} href="https://translate.google.com" target="_blank">
+                <img src="https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_42x16dp.png" width="37px" height="14px" className={classes.gooLogoImg} alt="Google Translate" />
+                <span className={classes.blackTranslateColor}>Translate</span>
+              </a>
+            </ListSubheader>
+            { langsList.map((lang,index) =>  
+              <LangMenuItem key={index} langName={lang.name} langCode={lang['1']} handleSelectLang={this.handleRequestCloseAfterSelect} />
+            )}
+          </List>
+        }
+        
       </div>
     );
   }
