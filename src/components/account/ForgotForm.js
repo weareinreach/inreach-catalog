25518208f2ea1225007d16fdf40184837d0faada
Jeremy Hatter <jeremy@grassriots.com>
2017-10-24 17:09:16 -0400

    fixed typo in TextField

--- src/components/account/ForgotForm.js
+++ src/components/account/ForgotForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import TextField from 'material-ui/Textfield';
+import TextField from 'material-ui/TextField';
 import Typography from 'material-ui/Typography';
 import {withStyles} from 'material-ui/styles';
 
