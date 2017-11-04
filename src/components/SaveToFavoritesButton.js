9d8106673711946c115665818468cfa43407fbe5
Ian Wright <ian@authentise.com>
2017-11-04 12:04:41 -0600

    Create SaveToFavoritesButton

---
+++ src/components/SaveToFavoritesButton.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles';
+
+import Button from 'material-ui/Button';
+import Menu, {MenuItem} from 'material-ui/Menu';
+import Typography from 'material-ui/Typography';
+
+import RedHeartIcon from './icons/RedHeartIcon';
+
+const styles = theme => ({
+  viewYourFavoritesText: {
+    color: theme.palette.secondary[500],
+    fontWeight: '300',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+class SaveToFavoritesButton extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      anchorEl: null,
+      open: false
+    };
+
+    this.handleMenuOpen = this.handleMenuOpen.bind(this);
+    this.handleMenuClose = this.handleMenuClose.bind(this);
+    this.handleSaveToFavorites = this.handleSaveToFavorites.bind(this);
+  }
+
+  handleMenuOpen(event) {
+    this.setState({ open: true, anchorEl: event.currentTarget });
+  };
+
+  handleMenuClose() {
+    this.setState({ open: false });
+  };
+
+  handleSaveToFavorites() {
+    console.log('click');
+  }
+
+  render() {
+    const { handleSaveToFavorites, handleMenuOpen, handleMenuClose } = this;
+    const { anchorEl, open } = this.state;
+    const { classes, user } = this.props;
+
+    return (
+      <div>
+        <Button onClick={user ? handleMenuOpen : handleSaveToFavorites}>
+          <Typography
+            type='display4'
+            className={classes.viewYourFavoritesText}
+          >
+            Save To Favorites
+            <RedHeartIcon width={'38px'}/>
+          </Typography>
+        </Button>
+        <Menu
+          id="favorites-menu"
+          anchorEl={anchorEl}
+          anchorOrigin={{vertical: 'bottom'}}
+          getContentAnchorEl={null}
+          open={open}
+          onRequestClose={handleMenuClose}>
+          <MenuItem onClick={handleMenuClose}>My List</MenuItem>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+SaveToFavoritesButton.defaultProps = { user: null };
+
+SaveToFavoritesButton.propTypes = {
+  classes: PropTypes.object.isRequired,
+  user: PropTypes.number,
+};
+
+export default withStyles(styles)(SaveToFavoritesButton);
