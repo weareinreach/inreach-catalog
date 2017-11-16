f0e0ce30b5e2c53be48bda8d76403be9ba56d4bf
Ian Wright <ian@authentise.com>
2017-11-16 14:54:28 -0700

    Refactor ListResources import ResourceListItem
    location was updated

--- src/components/favorites/ListResources.jsx
+++ src/components/favorites/ListResources.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import ResourceListItem from '../ResourceListItem';
+import ResourceListItem from '../resource/ResourceListItem';
 
 const ListResources = ({ handleRemoveFavorite, resources }) => (
   <div>
