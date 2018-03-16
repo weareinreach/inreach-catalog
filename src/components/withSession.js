c5d3fee05d651952f8f8545a0f5dc023d75a72ab
Ian Wright <ian@authentise.com>
2018-03-16 14:27:15 -0600

    Refactor code style for withSession
    with Prettier. Thanks Prettier

--- src/components/withSession.js
+++ src/components/withSession.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import fetch from 'node-fetch';
 import config from '../config/config.js';
-import {fetchUser, fetchUserLists} from '../helpers/odasRequests';
+import { fetchUser, fetchUserLists } from '../helpers/odasRequests';
 
 export default function withSession(WrappedComponent) {
   return class extends React.Component {
     constructor(props) {
-      super(props)
+      super(props);
 
       const jwt = window.localStorage.getItem('jwt');
       const user = parseInt(window.localStorage.getItem('user'));
@@ -43,42 +43,44 @@ export default function withSession(WrappedComponent) {
     }
 
     handleListNew(list) {
-      this.setState(prevState => ({ lists: [...prevState.lists, list]}));
-    };
+      this.setState(prevState => ({ lists: [...prevState.lists, list] }));
+    }
 
     handleListAddFavorite(listId, favorite) {
-      this.setState(prevState => ({ lists: prevState.lists.map(list => (
-        list.id === listId
-          ? Object.assign(
-              {},
-              list,
-              {fetchable_list_items: [
-                ...list.fetchable_list_items,
-                { fetchable_id: favorite}
-              ]},
-            )
-          : list
-      ))}));
-    };
+      this.setState(prevState => ({
+        lists: prevState.lists.map(
+          list =>
+            list.id === listId
+              ? Object.assign({}, list, {
+                  fetchable_list_items: [
+                    ...list.fetchable_list_items,
+                    { fetchable_id: favorite },
+                  ],
+                })
+              : list
+        ),
+      }));
+    }
 
     handleListRemoveFavorite(listId, favorite) {
-      this.setState(prevState => ({ lists: prevState.lists.map(list => (
-        list.id === listId
-          ? Object.assign(
-              {},
-              list,
-              {fetchable_list_items: list.fetchable_list_items.filter(item =>
-                item.fetchable_id !== favorite
-              )},
-            )
-          : list
-      ))}));
+      this.setState(prevState => ({
+        lists: prevState.lists.map(
+          list =>
+            list.id === listId
+              ? Object.assign({}, list, {
+                  fetchable_list_items: list.fetchable_list_items.filter(
+                    item => item.fetchable_id !== favorite
+                  ),
+                })
+              : list
+        ),
+      }));
     }
 
     fetchLists(session) {
       fetchUserLists(session)
         .then(data => {
-          this.setState({lists: data ? data.collections : []});
+          this.setState({ lists: data ? data.collections : [] });
         })
         .catch(error => {
           this.handleLogOut();
@@ -97,7 +99,7 @@ export default function withSession(WrappedComponent) {
     handleStorageChange() {
       this.setState({
         session: window.localStorage.getItem('jwt'),
-        user: parseInt(window.localStorage.getItem('user'))
+        user: parseInt(window.localStorage.getItem('user')),
       });
     }
 
