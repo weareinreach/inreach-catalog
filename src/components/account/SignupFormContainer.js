bb36efa6d5b82b0f7f4a12e00ed06ce38cf98327
Ian Wright <ian@authentise.com>
2018-05-06 15:15:07 -0600

    Update signup workflow in SignupFormContainer
    So that the organization selection happens in a new panel

--- src/components/account/SignupFormContainer.js
+++ src/components/account/SignupFormContainer.js
@@ -10,6 +10,7 @@ class SignupFormContainer extends React.Component {
     super(props);
 
     this.state = {
+      activeStep: 0,
       email: '',
       password: '',
       passwordConfirmation: '',
@@ -17,8 +18,11 @@ class SignupFormContainer extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleCreateAffiliation = this.handleCreateAffiliation.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleStepNext = this.handleStepNext.bind(this);
+    this.handleStepBack = this.handleStepBack.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
   }
 
   handleChange(event) {
@@ -28,14 +32,40 @@ class SignupFormContainer extends React.Component {
 
   handleSelect(selection) {
     this.setState({ selection });
+    this.handleStepNext();
   }
 
-  handleSubmit(event) {
+  handleStepNext() {
+    this.setState(prevState => ({ activeStep: prevState.activeStep + 1 }));
+  }
+
+  handleStepBack() {
+    this.setState(prevState => ({ activeStep: prevState.activeStep - 1 }));
+  }
+
+  handleCreateAffiliation(event) {
     event.preventDefault();
     const {
       handleMessageNew,
       handleRequestClose,
       organizationSelection,
+      session,
+    } = this.props;
+    const { id, name } = organizationSelection;
+    createAffiliation({ id, name }, session)
+      .then(() => handleRequestClose())
+      .catch(() => {
+        handleMessageNew(
+          `Sorry. Something went wrong connecting you to your organization.`
+        );
+      });
+  }
+
+  handleSignUp(event) {
+    event.preventDefault();
+    const {
+      handleMessageNew,
+      handleRequestClose,
     } = this.props;
     const { email, password, passwordConfirmation, selection } = this.state;
     const isProfessional = selection === 'provider';
@@ -48,27 +78,15 @@ class SignupFormContainer extends React.Component {
       handleMessageNew('The passwords you have entered do not match');
       return;
     }
-    //Temporarily make the organization field not required
-    if (false && selection === 'provider' && !organizationSelection) {
-      handleMessageNew('Please select an organization.');
-      return;
-    }
 
     createUser({ email, password, isProfessional })
       .then(({ jwt }) => {
         this.props.handleLogIn(jwt);
-        if (!organizationSelection) {
+        if (!isProfessional) {
           handleRequestClose();
+        } else {
+          this.handleStepNext();
         }
-        const { id, name } = organizationSelection;
-        createAffiliation({ id, name }, jwt)
-          .then(() => handleRequestClose())
-          .catch(() => {
-            handleMessageNew(
-              `Sorry. Something went wrong connecting you to your organization.`
-            );
-            handleRequestClose();
-          });
       })
       .catch(error => {
         error.response.status === 400
@@ -85,17 +103,25 @@ class SignupFormContainer extends React.Component {
         {...this.props}
         {...this.state}
         handleChange={this.handleChange}
+        handleCreateAffiliation={this.handleCreateAffiliation}
         handleSelect={this.handleSelect}
-        handleSubmit={this.handleSubmit}
+        handleSignUp={this.handleSignUp}
+        handleStepNext={this.handleStepNext}
+        handleStepBack={this.handleStepBack}
       />
     );
   }
 }
 
+SignupFormContainer.defaultProps = {
+  session: null,
+};
+
 SignupFormContainer.propTypes = {
   handleLogIn: PropTypes.func.isRequired,
   handleMessageNew: PropTypes.func.isRequired,
   handleRequestClose: PropTypes.func.isRequired,
+  session: PropTypes.string,
 };
 
 export default withOrganizations(SignupFormContainer);
