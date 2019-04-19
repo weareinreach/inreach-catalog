289d68084d0ccc6998d3f501d3139cb884b81fa0
Jeremy Hatter <jhatter@gmail.com>
2019-04-19 08:14:22 -0400

    add/fix back button on resources and services; fix mapping of service; handle share of services; bugfixes

--- src/components/AsylumConnectBackButton.js
+++ src/components/AsylumConnectBackButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ArrowBackIcon from 'material-ui-icons/ArrowBack';
+import BackIcon from './icons/BackIcon';
 import Button from 'material-ui/Button';
 import {withStyles} from 'material-ui/styles';
 
@@ -8,18 +8,32 @@ const styles = (theme) => ({
   buttonRoot: {
     minWidth: '0',
     padding: '0',
+    textTransform: 'none' 
   },
   'contrast': {
     color: theme.palette.common.white
   },
   'default': {
     color: theme.palette.common.darkBlack
+  },
+  'primary': {
+    color: theme.palette.primary[500],
+    '&:hover': {
+      color: theme.palette.primary[900]
+    }
+  },
+  'secondary': {
+    color: theme.palette.secondary[500],
+    '&:hover': {
+      color: theme.palette.secondary[900]
+    }
   }
 });
 
-const AsylumConnectBackButton = ({onClick, classes, color="default"}) => (
+const AsylumConnectBackButton = ({onClick, classes, width="24px", color="secondary", text=""}) => (
   <Button className={classes[color]} classes={{root: classes.buttonRoot}} onClick={onClick}>
-    <ArrowBackIcon />
+    <BackIcon width={width} color={'currentColor'} />
+    {text}
   </Button>
 );
 
