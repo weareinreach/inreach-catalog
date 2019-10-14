cccc3670e05b8f93f1447b6906c9b2907af20ae9
Jeremy Hatter <jhatter@gmail.com>
2019-10-14 06:50:21 -0400

    updated service type tag output on mobile and desktop

--- src/components/resource/DetailServiceType.js
+++ src/components/resource/DetailServiceType.js
@@ -14,29 +14,61 @@ import ACBadge from '../Badge';
 //http://localhost:8080/en_US/resource/larkin-street-youth-services/service/get-emergency-shelter-for-youth
 
 const ServiceType = (props) => {
-  var listedTags = [];
+  let listedTags = [];
+  let tagDetails = [];
+  let unfoundTags = [];
+  let subcategories = [];
+  let categories = [];
+  let list = false;
   const tags = resourceTypes.getResourceIndex(props.locale);
+  if(props.list && props.list.length) {  //console.log(props.list);
+    //find all of the tag objects for all tags
+    props.list.map(item => {
+      let tag = typeof tags[item] !== "undefined" ? tags[item] : item;
+      
+      if(tag.title) {
+        subcategories.push(tag);
+      } else if(tag.category) {
+        categories.push(tag);
+      } else {
+        unfoundTags.push(tag);
+      }
+      
+    });
+    //console.log(subcategories, categories, unfoundTags);
+    list = subcategories
+      .map(subcategory => ({label: subcategory.title, type: subcategory.type}))
+      .concat(
+        categories.length ? categories.filter(category => {
+          return subcategories.filter(subcategory => (subcategory.category === category.category)).length == 0
+        })
+        .map(category => ({label: category.title, type: category.type})) : [],
+        xunfoundTags.length ? unfoundTags.map(unfound => ({label: unfound, type: 'misc'})) : []
+    );
+    //console.log(list);
+  }
+
   return (
     <Grid container spacing={0}>
       <Grid item xs={12} className={props.classes.sectionSpacing}>
         <Grid container spacing={0}>
           <Grid item xs={12}>
-            {(props.list && props.list ?
-              props.list.map((item, index) => {
-                let tag = typeof tags[item] !== "undefined" ? (tags[item].title ? tags[item].title : tags[item].category) : item;
-                if(listedTags.indexOf(tag) < 0 && resourceTypes.getBadge([item]) !== 'misc') {
-                  listedTags.push(tag);
+            {(list ?
+              list.map((tag, index) => {
+                //let tag = typeof tags[item] !== "undefined" ? (tags[item].title ? tags[item].title : tags[item].category) : item;
+                if(listedTags.indexOf(tag.label) < 0 && tag.type !== 'misc') {
+                  listedTags.push(tag.label);
                   return (
                     <span key={index} style={{position:'relative'}} >
                       {(() => {
-                          let badge = resourceTypes.getBadge([item]);
+                          //let badge = resourceTypes.getBadge([tag]);
 
                           return (
-                            <ACBadge extraClasses={{icon: props.classes.serviceBadge,tooltip:props.classes.serviceTooltip}} key='misc' type={badge} width='48px' height='48px' />
+                            <ACBadge extraClasses={{icon: props.classes.serviceBadge,tooltip:props.classes.serviceTooltip}} type={tag.type} mobileLabel={tag.label} width='48px' height='48px' />
                           );
                         })()
                       }
-                      {props.isMobile ? null : <p className={props.classes.serviceText}>{tag}</p>}
+                      {props.isMobile ? null : <p className={props.classes.serviceText}>{tag.label}</p>}
                     </span>
                   )
                 } else {
