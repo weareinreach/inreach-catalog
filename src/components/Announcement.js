e12bb42ccde1981dfa1cd418c73e332763fe41f3
Romello Goodman <romellogood@gmail.com>
2020-04-08 13:31:42 -0400

    Updating the top banner

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -4,23 +4,23 @@ import PropTypes from 'prop-types';
 import Typography from 'material-ui/Typography';
 import {withStyles} from 'material-ui/styles';
 
-const styles = theme => ({
+const styles = (theme) => ({
   announcement: {
     backgroundColor: theme.palette.secondary[500],
     padding: '2rem 0',
-    textAlign: 'center'
+    textAlign: 'center',
   },
   pointer: {
     cursor: 'pointer',
     display: 'inline-block',
-    position: 'relative'
+    position: 'relative',
   },
   pointerText: {
     textShadow:
       '2px 0 ' +
       theme.palette.secondary[500] +
       ', -2px 0 ' +
-      theme.palette.secondary[500]
+      theme.palette.secondary[500],
   },
   underline: {
     position: 'absolute',
@@ -30,19 +30,19 @@ const styles = theme => ({
     border: 'none',
     height: '1px',
     background: 'white',
-    bottom: '.2em'
+    bottom: '.2em',
   },
   textContent: {
     color: theme.palette.common.darkWhite,
     maxWidth: theme.maxColumnWidth,
-    margin: '0 auto'
-  }
+    margin: '0 auto',
+  },
 });
 
 const Announcement = ({classes, handleRequestOpen}) => (
   <div className={classes.announcement + ' hide--on-print'}>
     <Typography variant="body1" className={classes.textContent}>
-      Asylum seekers contact service providers at their own risk.
+      Users contact service providers at their own risk.
       <br />
       <span>Please read our complete </span>
       <strong
@@ -67,7 +67,7 @@ const Announcement = ({classes, handleRequestOpen}) => (
 
 Announcement.propTypes = {
   classes: PropTypes.object.isRequired,
-  handleRequestOpen: PropTypes.func.isRequired
+  handleRequestOpen: PropTypes.func.isRequired,
 };
 
 export default withStyles(styles)(Announcement);
