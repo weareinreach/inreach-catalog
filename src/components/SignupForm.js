07431e7eb689a1d46c58a13095ec52676a5bb6b8
Romello Goodman <romellogood@gmail.com>
2020-04-13 07:50:04 -0400

    Updating signup text to request email based on type

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -62,151 +62,160 @@ const SignupForm = ({
   password,
   passwordConfirmation,
   selection,
-}) => (
-  <div className={classes.container}>
-    {activeStep === 0 && (
-      <div className={classes.container}>
-        <Typography className={classes.marginBottomLg} variant="h5">
-          Which are you?
-        </Typography>
-        <AsylumConnectButton
-          className={classes.marginBottom}
-          onClick={() => handleSelect(SEEKER_TYPE)}
-          variant="primary"
-        >
-          I am looking for help for myself
-        </AsylumConnectButton>
-        <AsylumConnectButton
-          className={classes.marginBottom}
-          onClick={() => handleSelect(LAWYER_TYPE)}
-          variant="primary"
-        >
-          I am an attorney or law student
-        </AsylumConnectButton>
-        <AsylumConnectButton
-          className={classes.marginBottomLg}
-          onClick={() => handleSelect(PROVIDER_TYPE)}
-          variant="primary"
-        >
-          I am a non-legal service provider
-        </AsylumConnectButton>
-        <div onClick={() => handleRequestOpen('login')}>
-          <Typography variant="body1">
-            <span className={classes.link}>Already have an account?</span>
+}) => {
+  const emailLabel =
+    selection === SEEKER_TYPE
+      ? 'Email'
+      : selection === LAWYER_TYPE
+      ? 'Firm, Organization or School Email'
+      : 'Organization Email';
+
+  return (
+    <div className={classes.container}>
+      {activeStep === 0 && (
+        <div className={classes.container}>
+          <Typography className={classes.marginBottomLg} variant="h5">
+            Which are you?
           </Typography>
-        </div>
-      </div>
-    )}
-    {activeStep === 1 && (
-      <form className={classes.container} onSubmit={handleSignUp}>
-        <TextField
-          id="email"
-          label={
-            selection === 'seeker'
-              ? 'Email'
-              : 'Firm, Organization or School Email'
-          }
-          margin="normal"
-          name="email"
-          onChange={handleChange}
-          required
-          type="email"
-          value={email}
-        />
-        <TextField
-          error={password.length > 0 && password.length < 8}
-          helperText={
-            password.length > 0 && password.length < 8
-              ? 'Password must be at least 8 characters.'
-              : null
-          }
-          id="password"
-          label="Password"
-          margin="normal"
-          name="password"
-          onChange={handleChange}
-          required
-          type="password"
-          value={password}
-        />
-        <TextField
-          id="passwordConfirmation"
-          label="Confirm Password"
-          margin="normal"
-          name="passwordConfirmation"
-          onChange={handleChange}
-          required
-          type="password"
-          value={passwordConfirmation}
-        />
-        <Typography variant="body1" className={classes.paddingVertical}>
-          By clicking "Sign Up," you agree to AsylumConnect's{` `}
-          <a
-            href="https://asylumconnect.org/privacy"
-            target="_blank"
-            rel="noopener noreferrer"
+          <AsylumConnectButton
+            className={classes.marginBottom}
+            onClick={() => handleSelect(SEEKER_TYPE)}
+            variant="primary"
           >
-            Privacy Policy
-          </a>
-          {` `}and{` `}
-          <a
-            href="https://asylumconnect.org/terms-of-use"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Terms of Use
-          </a>
-          .
-        </Typography>
-        <AsylumConnectButton variant="primary">Sign Up</AsylumConnectButton>
-      </form>
-    )}
-    {activeStep === 2 && (
-      <form onSubmit={handleCreateAffiliation}>
-        <Typography variant="h6">Connect to Your Organization</Typography>
-        <OrganizationAutocomplete
-          handleBlurOrganizations={handleBlurOrganizations}
-          handleMessageNew={handleMessageNew}
-          handleOrganizationSearchChange={handleOrganizationSearchChange}
-          handleOrganizationSelect={handleOrganizationSelect}
-          handleOrganizationsFetchRequested={handleOrganizationsFetchRequested}
-          handleOrganizationsClearRequested={handleOrganizationsClearRequested}
-          handleRequestClose={handleRequestClose}
-          isLoadingOrganizations={isLoadingOrganizations}
-          locale={locale}
-          organizationSearch={organizationSearch}
-          organizationSelection={organizationSelection}
-          organizations={organizations}
-        />
-        <div className={classes.marginVertical}>
-          <AsylumConnectButton variant="primary">
-            Join Organization
+            I am looking for help for myself
           </AsylumConnectButton>
+          <AsylumConnectButton
+            className={classes.marginBottom}
+            onClick={() => handleSelect(LAWYER_TYPE)}
+            variant="primary"
+          >
+            I am an attorney or law student
+          </AsylumConnectButton>
+          <AsylumConnectButton
+            className={classes.marginBottomLg}
+            onClick={() => handleSelect(PROVIDER_TYPE)}
+            variant="primary"
+          >
+            I am a non-legal service provider
+          </AsylumConnectButton>
+          <div onClick={() => handleRequestOpen('login')}>
+            <Typography variant="body1">
+              <span className={classes.link}>Already have an account?</span>
+            </Typography>
+          </div>
         </div>
-        <Typography variant="body1">
-          You may also join your organization later in account settings.
-        </Typography>
-      </form>
-    )}
-    <MobileStepper
-      className={classes.spacingTop + ' ' + classes.backgroundTransparent}
-      type="dots"
-      steps={selection === 'provider' ? 3 : 2}
-      position="static"
-      activeStep={activeStep}
-      nextButton={<div />}
-      backButton={<div />}
-    />
-    {activeStep === 1 && (
-      <div className={classes.flex}>
-        <Button dense onClick={handleStepBack}>
-          <KeyboardArrowLeft />
-          Back
-        </Button>
-      </div>
-    )}
-  </div>
-);
+      )}
+      {activeStep === 1 && (
+        <form className={classes.container} onSubmit={handleSignUp}>
+          <TextField
+            id="email"
+            label={emailLabel}
+            margin="normal"
+            name="email"
+            onChange={handleChange}
+            required
+            type="email"
+            value={email}
+          />
+          <TextField
+            error={password.length > 0 && password.length < 8}
+            helperText={
+              password.length > 0 && password.length < 8
+                ? 'Password must be at least 8 characters.'
+                : null
+            }
+            id="password"
+            label="Password"
+            margin="normal"
+            name="password"
+            onChange={handleChange}
+            required
+            type="password"
+            value={password}
+          />
+          <TextField
+            id="passwordConfirmation"
+            label="Confirm Password"
+            margin="normal"
+            name="passwordConfirmation"
+            onChange={handleChange}
+            required
+            type="password"
+            value={passwordConfirmation}
+          />
+          <Typography variant="body1" className={classes.paddingVertical}>
+            By clicking "Sign Up," you agree to AsylumConnect's{` `}
+            <a
+              href="https://asylumconnect.org/privacy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy Policy
+            </a>
+            {` `}and{` `}
+            <a
+              href="https://asylumconnect.org/terms-of-use"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Terms of Use
+            </a>
+            .
+          </Typography>
+          <AsylumConnectButton variant="primary">Sign Up</AsylumConnectButton>
+        </form>
+      )}
+      {activeStep === 2 && (
+        <form onSubmit={handleCreateAffiliation}>
+          <Typography variant="h6">Connect to Your Organization</Typography>
+          <OrganizationAutocomplete
+            handleBlurOrganizations={handleBlurOrganizations}
+            handleMessageNew={handleMessageNew}
+            handleOrganizationSearchChange={handleOrganizationSearchChange}
+            handleOrganizationSelect={handleOrganizationSelect}
+            handleOrganizationsFetchRequested={
+              handleOrganizationsFetchRequested
+            }
+            handleOrganizationsClearRequested={
+              handleOrganizationsClearRequested
+            }
+            handleRequestClose={handleRequestClose}
+            isLoadingOrganizations={isLoadingOrganizations}
+            locale={locale}
+            organizationSearch={organizationSearch}
+            organizationSelection={organizationSelection}
+            organizations={organizations}
+          />
+          <div className={classes.marginVertical}>
+            <AsylumConnectButton variant="primary">
+              Join Organization
+            </AsylumConnectButton>
+          </div>
+          <Typography variant="body1">
+            You may also join your organization later in account settings.
+          </Typography>
+        </form>
+      )}
+      <MobileStepper
+        className={classes.spacingTop + ' ' + classes.backgroundTransparent}
+        type="dots"
+        steps={selection === 'provider' ? 3 : 2}
+        position="static"
+        activeStep={activeStep}
+        nextButton={<div />}
+        backButton={<div />}
+      />
+      {activeStep === 1 && (
+        <div className={classes.flex}>
+          <Button dense onClick={handleStepBack}>
+            <KeyboardArrowLeft />
+            Back
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 SignupForm.defaultProps = {
   organizationSelection: null,
