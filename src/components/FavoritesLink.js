dd13fcefcbf38fd5dfae9cc8106eb40f2fe64fb5
FJKhan <fjgenus@gmail.com>
2021-02-10 09:55:38 -0500

    refactors files for L10N

--- src/components/FavoritesLink.js
+++ src/components/FavoritesLink.js
@@ -2,29 +2,33 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
-
 import Typography from '@material-ui/core/Typography';
+import {FormattedMessage} from 'react-intl';
 
 const styles = (theme) => ({
-  viewYourFavoritesText: {
-    color: theme.palette.secondary[500],
-    '&:hover': {
-      color: theme.palette.secondary[900],
-    },
-    fontWeight: theme.typography.fontWeightMedium,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+	viewYourFavoritesText: {
+		color: theme.palette.secondary[500],
+		'&:hover': {
+			color: theme.palette.secondary[900]
+		},
+		fontWeight: theme.typography.fontWeightMedium,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
 });
 
 const FavoritesLink = ({classes, locale}) => (
-  <Link to={'/' + locale + '/favorites'} className="hide--on-print">
-    <Typography type="h4" className={classes.viewYourFavoritesText}>
-      View Your Favorites
-    </Typography>
-  </Link>
+	<Link to={'/' + locale + '/favorites'} className="hide--on-print">
+		<FormattedMessage id="favorites.view-favorites">
+			{(favorites) => (
+				<Typography type="h4" className={classes.viewYourFavoritesText}>
+					{favorites}
+				</Typography>
+			)}
+		</FormattedMessage>
+	</Link>
 );
 
 FavoritesLink.propTypes = {classes: PropTypes.object.isRequired};
