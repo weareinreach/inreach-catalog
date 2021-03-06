ec9d136c62f2b920f93985344264769d44531316
FJKhan <fjgenus@gmail.com>
2021-03-06 14:11:01 -0500

    fixes mui props issue

--- src/components/ListNewForm.js
+++ src/components/ListNewForm.js
@@ -8,45 +8,45 @@ import {withStyles} from '@material-ui/core/styles';
 import AsylumConnectButton from './AsylumConnectButton';
 
 const styles = (theme) => ({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    textAlign: 'center',
-  },
-  helperText: {
-    lineHeight: '1.54',
-  },
-  spacingAbove: {marginTop: '3rem'},
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		textAlign: 'center'
+	},
+	helperText: {
+		lineHeight: '1.54'
+	},
+	spacingAbove: {marginTop: '3rem'}
 });
 
 const ListNewForm = ({classes, handleChange, handleSubmit, name, password}) => (
-  <form className={classes.container} onSubmit={handleSubmit}>
-    <Typography variant="body1">
-      Your favorites lists are only visible to you and anyone you share
-      them&nbsp;with.
-    </Typography>
-    <TextField
-      id="name"
-      label="List name"
-      margin="normal"
-      helperText="You could name your list by category, by day of the week, or by the name of whoever this list is for."
-      helperTextClassName={classes.helperText}
-      name="name"
-      onChange={handleChange}
-      required
-      value={name}
-    />
-    <AsylumConnectButton className={classes.spacingAbove} variant="primary">
-      Create New List
-    </AsylumConnectButton>
-  </form>
+	<form className={classes.container} onSubmit={handleSubmit}>
+		<Typography variant="body1">
+			Your favorites lists are only visible to you and anyone you share
+			them&nbsp;with.
+		</Typography>
+		<TextField
+			id="name"
+			label="List name"
+			margin="normal"
+			helperText="You could name your list by category, by day of the week, or by the name of whoever this list is for."
+			classes={classes.helperText}
+			name="name"
+			onChange={handleChange}
+			required
+			value={name}
+		/>
+		<AsylumConnectButton className={classes.spacingAbove} variant="primary">
+			Create New List
+		</AsylumConnectButton>
+	</form>
 );
 
 ListNewForm.propTypes = {
-  classes: PropTypes.object.isRequired,
-  handleChange: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  name: PropTypes.string.isRequired,
+	classes: PropTypes.object.isRequired,
+	handleChange: PropTypes.func.isRequired,
+	handleSubmit: PropTypes.func.isRequired,
+	name: PropTypes.string.isRequired
 };
 
 export default withStyles(styles)(ListNewForm);
