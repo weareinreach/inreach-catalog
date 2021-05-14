1c35a2120bcdb40d3bf548ae6937852e30384a44
Alfred <devops.moreira@gmail.com>
2021-05-13 22:28:48 -0600

    Login and Log out automation

--- cypress/integration/home_page_login_form_spec.js
+++ cypress/integration/home_page_login_form_spec.js
@@ -11,9 +11,9 @@ describe('Home Page Login Form Suite of Tests', () => {
         cy.fixture('user_new.json').as('new_user');
     });
     afterEach(() => {
-		//Do the clean up
-		cy.deleteUsersIfExist();
-	});
+        //Do the clean up
+        cy.deleteUsersIfExist();
+    });
 
     //Root
     it('Root Test - Visual', () => {
@@ -22,86 +22,86 @@ describe('Home Page Login Form Suite of Tests', () => {
 
     //Login Form
     //Desktop
-    context('Desktop Version of the application',()=>{
-        it('Login Form Components',()=>{
-            cy.getElementByTestId('nav-account-sign-in').then($element =>{
+    context('Desktop Version of the application', () => {
+        it('Login Form Components', () => {
+            cy.getElementByTestId('nav-account-sign-in').then($element => {
                 expect($element).to.be.visible;
                 //click
                 cy.wrap($element).click();
                 cy.getElementByTestId('log-in-dialog-container').should('be.visible');
-                cy.getElementByTestId('log-in-dialog-container-title').then($element=>{
+                cy.getElementByTestId('log-in-dialog-container-title').then($element => {
                     expect($element).contain("Log In");
                     expect($element).to.be.visible;
                 });
                 cy.getElementByTestId('log-in-dialog-container-login-form').should('be.visible');
-                cy.getElementByTestId('log-in-dialog-container-email-input').then($element=>{
+                cy.getElementByTestId('log-in-dialog-container-email-input').then($element => {
                     expect($element).to.be.visible;
                     expect($element.children()).contain("Email");
                 });
-                cy.getElementByTestId('log-in-dialog-container-password-input').then($element=>{
+                cy.getElementByTestId('log-in-dialog-container-password-input').then($element => {
                     expect($element).to.be.visible;
                     expect($element.children()).contain("Password");
                 });
-                cy.getElementByTestId('log-in-dialog-container-privacy').then($element=>{
+                cy.getElementByTestId('log-in-dialog-container-privacy').then($element => {
                     expect($element).to.be.visible;
-                    expect($element).to.have.attr('href','https://asylumconnect.org/privacy');
+                    expect($element).to.have.attr('href', 'https://asylumconnect.org/privacy');
                     expect($element).contain("Privacy Policy");
-                    expect($element).to.have.attr('target','_blank');
-                    expect($element).to.have.attr('rel','noopener noreferrer');
+                    expect($element).to.have.attr('target', '_blank');
+                    expect($element).to.have.attr('rel', 'noopener noreferrer');
                 });
-                cy.getElementByTestId('log-in-dialog-container-terms-of-use').then($element=>{
+                cy.getElementByTestId('log-in-dialog-container-terms-of-use').then($element => {
                     expect($element).to.be.visible;
-                    expect($element).to.have.attr('href','https://asylumconnect.org/terms-of-use');
+                    expect($element).to.have.attr('href', 'https://asylumconnect.org/terms-of-use');
                     expect($element).contain("Terms of Use");
-                    expect($element).to.have.attr('target','_blank');
-                    expect($element).to.have.attr('rel','noopener noreferrer');
+                    expect($element).to.have.attr('target', '_blank');
+                    expect($element).to.have.attr('rel', 'noopener noreferrer');
                 });
-                cy.getElementByTestId('log-in-dialog-container-sign-in-button').then($element=>{
+                cy.getElementByTestId('log-in-dialog-container-sign-in-button').then($element => {
                     expect($element).to.be.visible;
-                    expect($element).to.have.attr('type','submit');
+                    expect($element).to.have.attr('type', 'submit');
                     expect($element.children()).contain("Sign In");
                 });
-                cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element=>{
+                cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element => {
                     expect($element).to.be.visible;
                     expect($element).contain("Forgot Password?");
                 });
-                cy.getElementByTestId('log-in-dialog-container-no-acount').then($element=>{
+                cy.getElementByTestId('log-in-dialog-container-no-acount').then($element => {
                     expect($element).to.be.visible;
                     expect($element.children()).contain("Don't have an account?");
                 });
 
+            });
         });
-    });
 
-        it.only('Login And Log Out Actions',()=>{
-            cy.getElementByTestId('nav-account-sign-in').then($element =>{
+        it('Login And Log Out Actions', () => {
+            cy.getElementByTestId('nav-account-sign-in').then($element => {
                 //click
                 cy.wrap($element).click();
-                
+
                 //Create User
-                cy.get('@new_user').then(user =>{
-                    cy.addUser(user).then(addedUserResponse =>{
+                cy.get('@new_user').then(user => {
+                    cy.addUser(user).then(addedUserResponse => {
                         cy.getElementByTestId('log-in-dialog-container-email-input').type(user.email);
                         cy.getElementByTestId('log-in-dialog-container-password-input').type(user.password);
                         cy.getElementByTestId('log-in-dialog-container-sign-in-button').click();
                         //Logeed In
-                        cy.getElementByTestId('nav-account-account-settings').then($element =>{
+                        cy.getElementByTestId('nav-account-account-settings').then($element => {
                             expect($element).to.be.visible;
-                            expect($element).to.have.attr('href','/en_US/account');
+                            expect($element).to.have.attr('href', '/en_US/account');
                             expect($element.children()).contain("Account Settings");
                         });
-                        cy.getElementByTestId('nav-account-sign-out').then($element =>{
+                        cy.getElementByTestId('nav-account-sign-out').then($element => {
                             expect($element).to.be.visible;
-                            expect($element).to.have.attr('href','/');
+                            expect($element).to.have.attr('href', '/');
                             expect($element.children()).contain("Sign Out");
                         });
                         //Logged Out
                         cy.getElementByTestId('nav-account-sign-out').click();
-                        cy.getElementByTestId('nav-account-sign-in').then($element=>{
+                        cy.getElementByTestId('nav-account-sign-in').then($element => {
                             expect($element).to.be.visible;
                             expect($element.children()).contain("Sign In");
                         });
-                        cy.getElementByTestId('nav-account-sign-up').then($element=>{
+                        cy.getElementByTestId('nav-account-sign-up').then($element => {
                             expect($element).to.be.visible;
                             expect($element.children()).contain("Sign Up");
                         });
@@ -113,4 +113,185 @@ describe('Home Page Login Form Suite of Tests', () => {
         });
     });
 
+    context('Tablet Version of the application', () => {
+        it('Login Form Components', () => {
+            cy.viewport('ipad-2');
+            cy.getElementByTestId('nav-account-sign-in').then($element => {
+                expect($element).to.be.visible;
+                //click
+                cy.wrap($element).click({force:true});
+                cy.getElementByTestId('log-in-dialog-container').should('be.visible');
+                cy.getElementByTestId('log-in-dialog-container-title').then($element => {
+                    expect($element).contain("Log In");
+                    expect($element).to.be.visible;
+                });
+                cy.getElementByTestId('log-in-dialog-container-login-form').should('be.visible');
+                cy.getElementByTestId('log-in-dialog-container-email-input').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element.children()).contain("Email");
+                });
+                cy.getElementByTestId('log-in-dialog-container-password-input').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element.children()).contain("Password");
+                });
+                cy.getElementByTestId('log-in-dialog-container-privacy').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element).to.have.attr('href', 'https://asylumconnect.org/privacy');
+                    expect($element).contain("Privacy Policy");
+                    expect($element).to.have.attr('target', '_blank');
+                    expect($element).to.have.attr('rel', 'noopener noreferrer');
+                });
+                cy.getElementByTestId('log-in-dialog-container-terms-of-use').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element).to.have.attr('href', 'https://asylumconnect.org/terms-of-use');
+                    expect($element).contain("Terms of Use");
+                    expect($element).to.have.attr('target', '_blank');
+                    expect($element).to.have.attr('rel', 'noopener noreferrer');
+                });
+                cy.getElementByTestId('log-in-dialog-container-sign-in-button').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element).to.have.attr('type', 'submit');
+                    expect($element.children()).contain("Sign In");
+                });
+                cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element).contain("Forgot Password?");
+                });
+                cy.getElementByTestId('log-in-dialog-container-no-acount').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element.children()).contain("Don't have an account?");
+                });
+            });
+        });
+
+        it('Login And Log Out Actions', () => {
+            cy.viewport('ipad-2');
+            cy.getElementByTestId('nav-account-sign-in').then($element => {
+                //click
+                cy.wrap($element).click({force:true});
+
+                //Create User
+                cy.get('@new_user').then(user => {
+                    cy.addUser(user).then(addedUserResponse => {
+                        cy.getElementByTestId('log-in-dialog-container-email-input').type(user.email);
+                        cy.getElementByTestId('log-in-dialog-container-password-input').type(user.password);
+                        cy.getElementByTestId('log-in-dialog-container-sign-in-button').click();
+                        //Logeed In
+                        cy.getElementByTestId('nav-account-account-settings').then($element => {
+                            expect($element).to.be.visible;
+                            expect($element).to.have.attr('href', '/en_US/account');
+                            expect($element.children()).contain("Account Settings");
+                        });
+                        cy.getElementByTestId('nav-account-sign-out').then($element => {
+                            expect($element).to.be.visible;
+                            expect($element).to.have.attr('href', '/');
+                            expect($element.children()).contain("Sign Out");
+                        });
+                        //Logged Out
+                        cy.getElementByTestId('nav-account-sign-out').click();
+                        cy.getElementByTestId('nav-account-sign-in').then($element => {
+                            expect($element).to.be.visible;
+                            expect($element.children()).contain("Sign In");
+                        });
+                        cy.getElementByTestId('nav-account-sign-up').then($element => {
+                            expect($element).to.be.visible;
+                            expect($element.children()).contain("Sign Up");
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    context('Mobile Version of the application',()=>{
+        it.only('Login Form Components',()=>{
+            cy.viewport('iphone-x');
+            cy.getElementByTestId('mobile-nav-button-account').then($element =>{
+                expect($element).to.be.visible;
+                //click
+                cy.wrap($element).click();
+                cy.getElementByTestId('log-in-dialog-container-login-form').should('be.visible');
+                cy.getElementByTestId('log-in-dialog-container-email-input').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element.children()).contain("Email");
+                });
+                cy.getElementByTestId('log-in-dialog-container-password-input').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element.children()).contain("Password");
+                });
+                cy.getElementByTestId('log-in-dialog-container-privacy').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element).to.have.attr('href', 'https://asylumconnect.org/privacy');
+                    expect($element).contain("Privacy Policy");
+                    expect($element).to.have.attr('target', '_blank');
+                    expect($element).to.have.attr('rel', 'noopener noreferrer');
+                });
+                cy.getElementByTestId('log-in-dialog-container-terms-of-use').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element).to.have.attr('href', 'https://asylumconnect.org/terms-of-use');
+                    expect($element).contain("Terms of Use");
+                    expect($element).to.have.attr('target', '_blank');
+                    expect($element).to.have.attr('rel', 'noopener noreferrer');
+                });
+                cy.getElementByTestId('log-in-dialog-container-sign-in-button').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element).to.have.attr('type', 'submit');
+                    expect($element.children()).contain("Sign In");
+                });
+                cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element).contain("Forgot Password?");
+                });
+                cy.getElementByTestId('log-in-dialog-container-no-acount').then($element => {
+                    expect($element).to.be.visible;
+                    expect($element.children()).contain("Don't have an account?");
+                });
+            })
+        });
+        it('Login And Log Out Actions', () => {
+            cy.viewport('iphone-x');
+            cy.getElementByTestId('mobile-nav-button-account').then($element => {
+                //click
+                cy.wrap($element).click({force:true});
+
+                //Create User
+                cy.get('@new_user').then(user => {
+                    cy.addUser(user).then(addedUserResponse => {
+                        cy.getElementByTestId('log-in-dialog-container-email-input').type(user.email);
+                        cy.getElementByTestId('log-in-dialog-container-password-input').type(user.password);
+                        cy.getElementByTestId('log-in-dialog-container-sign-in-button').click();
+                        //Logeed In
+                        cy.getElementByTestId('account-page-header').then($element=>{
+                            expect($element).to.be.visible;
+                            expect($element.children()).contain("Your Account"); 
+                        });
+                        cy.getElementByTestId('account-page-mobile-tabs').should('be.visible');
+                        cy.getElementByTestId('account-page-mobile-tab-your-account').then($element=>{
+                            expect($element).to.be.visible;
+                            expect($element.children()).contain("Your Account");
+                        });
+                        cy.getElementByTestId('account-page-mobile-email').then($element=>{
+                            expect($element).to.be.visible;
+                            expect($element.children()).contain("Change Email Address");
+                        });
+                        cy.getElementByTestId('account-page-mobile-change-password').then($element=>{
+                            expect($element).to.be.visible;
+                            expect($element.children()).contain("Change Password");
+                        });
+                        cy.getElementByTestId('account-page-mobile-delete-account').then($element=>{
+                            expect($element).to.be.visible;
+                            expect($element.children()).contain("Delete Account");
+                        });
+                        //Logout
+                        cy.getElementByTestId('account-page-mobile-logout').then($element=>{
+                            expect($element).to.be.visible;
+                            expect($element.children()).contain("Logout");
+                            cy.wrap($element).click();
+                        });
+                        
+                    });
+                });
+            });
+        });
+    });
 });
