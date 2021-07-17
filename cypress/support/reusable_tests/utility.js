3e69fae71f87dd67eacd6c346d3257ea3564011e
Alfred <devops.moreira@gmail.com>
2021-07-17 13:06:00 -0600

    adding more test to utils

--- cypress/support/reusable_tests/utility.js
+++ cypress/support/reusable_tests/utility.js
@@ -8,9 +8,13 @@ Cypress.Commands.add('testUtilityFunctions',(utilObject)=>{
 
 function convertTimeTests(utilObject){
     expect(convertTime('16:00',utilObject.twelve_hour_format)).to.be.eq('4:00 PM');
+    expect(convertTime('09:00',utilObject.twelve_hour_format)).to.be.eq('9:00 AM');
     expect(convertTime('16:00',null)).to.be.eq('4:00 PM');
+    expect(convertTime('09:00',null)).to.be.eq('9:00 AM');
 
     expect(convertTime('16:00',utilObject.twenty_four_hour_format)).to.be.eq('16:00');
+    expect(convertTime('09:00',utilObject.twenty_four_hour_format)).to.be.eq('09:00');
     expect(convertTime('16:00',null)).to.be.eq('16:00');
+    expect(convertTime('09:00',null)).to.be.eq('09:00');
 }
 
