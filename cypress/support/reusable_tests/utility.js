903e30b0b7e4c54c0082ba0761cee50261fb1f7f
Alfred <devops.moreira@gmail.com>
2021-07-21 09:53:43 -0600

    increase coverage

--- cypress/support/reusable_tests/utility.js
+++ cypress/support/reusable_tests/utility.js
@@ -7,13 +7,25 @@ Cypress.Commands.add('testUtilityFunctions',(utilObject)=>{
 
 function convertTimeTests(utilObject){
     expect(convertTime('16:00',utilObject.twelve_hour_format)).to.be.eq('4:00 PM');
+    expect(convertTime('16:00:00',utilObject.twelve_hour_format)).to.be.eq('4:00 PM');
     expect(convertTime('09:00',utilObject.twelve_hour_format)).to.be.eq('9:00 AM');
-    expect(convertTime('16:00',null)).to.be.eq('4:00 pm');
-    expect(convertTime('09:00',null)).to.be.eq('9:00 am');
-
+    expect(convertTime('9:0',utilObject.twelve_hour_format)).to.be.eq('9:00 AM');
+    expect(convertTime('9 am',utilObject.twelve_hour_format)).to.be.eq('9:00 A');
+    expect(convertTime('4 pm',utilObject.twelve_hour_format)).to.be.eq('16:00 A');
+    expect(convertTime('9:0:00',utilObject.twelve_hour_format)).to.be.eq('9:00 AM');
+   
     expect(convertTime('16:00',utilObject.twenty_four_hour_format)).to.be.eq('16:00');
     expect(convertTime('09:00',utilObject.twenty_four_hour_format)).to.be.eq('09:00');
-    expect(convertTime('16:00',null)).to.be.eq('16:00');
-    expect(convertTime('09:00',null)).to.be.eq('09:00');
+    expect(convertTime('09:00:00',utilObject.twenty_four_hour_format)).to.be.eq('09:00');
+    expect(convertTime('9:0',utilObject.twenty_four_hour_format)).to.be.eq('09:00');
+    expect(convertTime('9:0:0',utilObject.twenty_four_hour_format)).to.be.eq('09:00');
+    expect(convertTime('9 am',utilObject.twenty_four_hour_format)).to.be.eq('09:00');
+    expect(convertTime('4 pm',utilObject.twenty_four_hour_format)).to.be.eq('04:00');
+
+    expect(convertTime('16:00',null)).to.be.eq('4:00 pm');
+    expect(convertTime('16:00:00',null)).to.be.eq('4:00 pm');
+    expect(convertTime('09:00',null)).to.be.eq('9:00 am');
+    expect(convertTime('9:0',null)).to.be.eq('9:00 am');
+    expect(convertTime('9:0:00',null)).to.be.eq('9:00 am');
 }
 
