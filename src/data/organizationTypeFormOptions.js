a55b8c99f0d95ad0899842a66b028aec9ce99fb0
dlg <>
2021-12-07 00:39:27 -0800

    use formatted id for labels and some values

--- src/data/organizationTypeFormOptions.js
+++ src/data/organizationTypeFormOptions.js
@@ -1,19 +1,60 @@
 export const organizationTypesLawyer = [
-	{value: 'Corporate law firm', testId: 'corp'},
-	{value: 'Law school student clinic', testId: 'law'},
-	{value: 'Legal nonprofit', testId: 'non-profit'},
-	{value: 'Other (specify)', testId: 'other'}
+	{
+		value: 'Corporate law firm',
+		testId: 'corp',
+		formatMessageId: 'account.sign-up-orgType-answer-corp'
+	},
+	{
+		value: 'Law school student clinic',
+		testId: 'law',
+		formatMessageId: 'account.sign-up-orgType-answer-law'
+	},
+	{
+		value: 'Legal nonprofit',
+		testId: 'legal-non-profit',
+		formatMessageId: 'account.sign-up-orgType-answer-legal-non-profit'
+	},
+	{
+		value: 'Other (specify)',
+		testId: 'other',
+		formatMessageId: 'aboutyou.answer-other'
+	}
 ];
 
 export const organizationTypesProvider = [
-	{value: 'Healthcare Provider', testId: 'healthcare'},
-	{value: 'Government Agency', testId: 'government'},
-	{value: 'Nonprofit', testId: 'non-profit'},
-	{value: 'Grassroots Direct Services Org', testId: 'grassroots'},
-	{value: 'Student Club', testId: 'student'},
+	{
+		value: 'Healthcare Provider',
+		testId: 'healthcare',
+		formatMessageId: 'account.sign-up-orgType-answer-healthcare'
+	},
+	{
+		value: 'Government Agency',
+		testId: 'government',
+		formatMessageId: 'account.sign-up-orgType-answer-government'
+	},
+	{
+		value: 'Nonprofit',
+		testId: 'non-profit',
+		formatMessageId: 'account.sign-up-orgType-answer-non-profit'
+	},
+	{
+		value: 'Grassroots Direct Services Org',
+		testId: 'grassroots',
+		formatMessageId: 'account.sign-up-orgType-answer-grassroots'
+	},
+	{
+		value: 'Student Club',
+		testId: 'student',
+		formatMessageId: 'account.sign-up-orgType-answer-student'
+	},
 	{
 		value: "N/A (I'm looking for safe resources for friends or family)",
-		testId: 'na'
+		testId: 'na',
+		formatMessageId: 'account.sign-up-orgType-answer-na'
 	},
-	{value: 'Other (specify)', testId: 'other'}
+	{
+		value: 'Other (specify)',
+		testId: 'other',
+		formatMessageId: 'aboutyou.answer-other'
+	}
 ];
