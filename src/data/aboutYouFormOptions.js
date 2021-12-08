b2bd3f353bc211dbe72633fc1ff89a7507551c72
dlg <>
2021-12-07 16:48:51 -0800

    cleaning up data files fro about you screens

--- src/data/aboutYouFormOptions.js
+++ src/data/aboutYouFormOptions.js
@@ -1,36 +1,29 @@
 export const aboutYouImmigrationOptions = [
 	{
-		value: 'Asylum seeker',
 		testId: 'asylum-seeker',
 		formatMessageId: 'aboutyou.immigration-answer-asylum-seeker'
 	},
 	{
-		value: 'Asylee (granted asylum)',
 		testId: 'asylee',
 		formatMessageId: 'aboutyou.immigration-answer-asylee'
 	},
 	{
-		value: 'Dreamer (DACA recipient)',
 		testId: 'dreamer',
 		formatMessageId: 'aboutyou.immigration-answer-dreamer'
 	},
 	{
-		value: 'Refugee',
 		testId: 'refugee',
 		formatMessageId: 'aboutyou.immigration-answer-refugee'
 	},
 	{
-		value: 'Immigrant',
 		testId: 'immigrant',
 		formatMessageId: 'aboutyou.immigration-answer-immigrant'
 	},
 	{
-		value: 'None of these apply to me',
 		testId: 'none',
 		formatMessageId: 'aboutyou.answer-none-apply'
 	},
 	{
-		value: 'Prefer not to say',
 		testId: 'prefer',
 		formatMessageId: 'aboutyou.answer-prefer-not-to-say'
 	}
@@ -38,52 +31,42 @@ export const aboutYouImmigrationOptions = [
 
 export const aboutYouCountryOptions = [
 	{
-		value: 'Africa',
 		testId: 'africa',
 		formatMessageId: 'aboutyou.country-answer-africa'
 	},
 	{
-		value: 'Asia',
 		testId: 'asia',
 		formatMessageId: 'aboutyou.country-answer-asia'
 	},
 	{
-		value: 'South America',
 		testId: 'south-america',
 		formatMessageId: 'aboutyou.country-answer-south-america'
 	},
 	{
-		value: 'Caribbean Islands',
 		testId: 'caribbean',
 		formatMessageId: 'aboutyou.country-answer-caribbean'
 	},
 	{
-		value: 'North America',
 		testId: 'north-america',
 		formatMessageId: 'aboutyou.country-answer-north-america'
 	},
 	{
-		value: 'Central America',
 		testId: 'central-america',
 		formatMessageId: 'aboutyou.country-answer-central-america'
 	},
 	{
-		value: 'Middle East',
 		tetId: 'middle-east',
 		formatMessageId: 'aboutyou.country-answer-middle-east'
 	},
 	{
-		value: 'Europe',
 		testId: 'europe',
 		formatMessageId: 'aboutyou.country-answer-europe'
 	},
 	{
-		value: 'Prefer not to say',
 		testId: 'prefer',
 		formatMessageId: 'aboutyou.answer-prefer-not-to-say'
 	},
 	{
-		value: 'Other (specify)',
 		testId: 'other',
 		formatMessageId: 'aboutyou.answer-other'
 	}
@@ -91,82 +74,66 @@ export const aboutYouCountryOptions = [
 
 export const aboutYouSogOptions = [
 	{
-		value: 'Lesbian',
 		testId: 'lesbian',
 		formatMessageId: 'aboutyou.identity-answer-lesbian'
 	},
 	{
-		value: 'Gay',
 		testId: 'gay',
 		formatMessageId: 'aboutyou.identity-answer-gay'
 	},
 	{
-		value: 'Bisexual',
 		testId: 'bisexual',
 		formatMessageId: 'aboutyou.identity-answer-bisexual'
 	},
 	{
-		value: 'Transgender',
 		testId: 'transgender',
 		formatMessageId: 'aboutyou.identity-answer-transgender'
 	},
 	{
-		value: 'Queer',
 		testId: 'queer',
 		formatMessageId: 'aboutyou.identity-answer-queer'
 	},
 	{
-		value: 'Questioning',
 		testId: 'questioning',
 		formatMessageId: 'aboutyou.identity-answer-questioning'
 	},
 	{
-		value: 'Intersex',
 		testId: 'intersex',
 		formatMessageId: 'aboutyou.identity-answer-intersex'
 	},
 	{
-		value: 'Asexual',
 		testId: 'asexual',
 		formatMessageId: 'aboutyou.identity-answer-asexual'
 	},
 	{
-		value: 'Pansexual',
 		testId: 'pansexual',
 		formatMessageId: 'aboutyou.identity-answer-pansexual'
 	},
 	{
-		value: 'Nonbinary',
 		testId: 'nonbinary',
 		formatMessageId: 'aboutyou.identity-answer-nonbinary'
 	},
 	{
-		value: 'Ally',
 		testId: 'ally',
 		formatMessageId: 'aboutyou.identity-answer-ally'
 	},
 	{
-		value: 'Straight',
 		testId: 'straight',
 		formatMessageId: 'aboutyou.identity-answer-straight'
 	},
 	{
-		value: 'Woman',
 		testId: 'woman',
 		formatMessageId: 'aboutyou.identity-answer-woman'
 	},
 	{
-		value: 'Man',
 		testId: 'man',
 		formatMessageId: 'aboutyou.identity-answer-man'
 	},
 	{
-		value: 'Prefer not to say',
 		testId: 'prefer',
 		formatMessageId: 'aboutyou.answer-prefer-not-to-say'
 	},
 	{
-		value: 'Other (specify)',
 		testId: 'other',
 		formatMessageId: 'aboutyou.answer-other'
 	}
@@ -174,77 +141,64 @@ export const aboutYouSogOptions = [
 
 export const aboutYouEthnicityOptions = [
 	{
-		value: 'African, Middle Eastern',
 		testId: 'af-md',
 		formatMessageId: 'aboutyou.ehnicity-answer-af-md'
 	},
 	{
-		value: 'African American, Black',
 		testId: 'af-am',
 		formatMessageId: 'aboutyou.ehnicity-answer-af-am'
 	},
 	{
-		value: 'Afro Caribbean, Black',
 		testId: 'af-car',
 		formatMessageId: 'aboutyou.ehnicity-answer-af-car'
 	},
 	{
-		value: 'Asian, Asian American',
 		testId: 'asian',
 		formatMessageId: 'aboutyou.ehnicity-answer-asian'
 	},
 	{
-		value: 'Hispanic/Latinx',
 		testId: 'hispanic',
 		formatMessageId: 'aboutyou.ehnicity-answer-hispanic'
 	},
 	{
-		value: 'Indian',
 		testId: 'indian',
 		formatMessageId: 'aboutyou.ehnicity-answer-indian'
 	},
 	{
-		value: 'Indigenous, Native Person',
 		testId: 'native',
 		formatMessageId: 'aboutyou.ehnicity-answer-native'
 	},
 	{
-		value: 'Pacific Islander',
 		testId: 'pacific',
 		formatMessageId: 'aboutyou.ehnicity-answer-pacific'
 	},
 	{
-		value: 'White, Caucasian',
 		testId: 'white',
 		formatMessageId: 'aboutyou.ehnicity-answer-white'
 	},
 	{
-		value: 'Prefer not to say',
 		testId: 'prefer',
 		formatMessageId: 'aboutyou.answer-prefer-not-to-say'
 	},
 	{
-		value: 'Other (specify)',
 		testId: 'other',
 		formatMessageId: 'aboutyou.answer-other'
 	}
 ];
 
 export const aboutYouAgeOptions = [
-	{value: 'Under 18', testId: '18', formatMessageId: 'aboutyou.age-answer-18'},
-	{value: '18-25', testId: '25', formatMessageId: 'aboutyou.age-answer-25'},
-	{value: '26-35', testId: '35', formatMessageId: 'aboutyou.age-answer-35'},
-	{value: '36-45', testId: '45', formatMessageId: 'aboutyou.age-answer-45'},
-	{value: '46-55', testId: '55', formatMessageId: 'aboutyou.age-answer-55'},
-	{value: '56-65', testId: '65', formatMessageId: 'aboutyou.age-answer-65'},
-	{value: '66-75', testId: '75', formatMessageId: 'aboutyou.age-answer-75'},
+	{testId: '18', formatMessageId: 'aboutyou.age-answer-18'},
+	{testId: '25', formatMessageId: 'aboutyou.age-answer-25'},
+	{testId: '35', formatMessageId: 'aboutyou.age-answer-35'},
+	{testId: '45', formatMessageId: 'aboutyou.age-answer-45'},
+	{testId: '55', formatMessageId: 'aboutyou.age-answer-55'},
+	{testId: '65', formatMessageId: 'aboutyou.age-answer-65'},
+	{testId: '75', formatMessageId: 'aboutyou.age-answer-75'},
 	{
-		value: '76 and older',
 		testId: 'older',
 		formatMessageId: 'aboutyou.age-answer-older'
 	},
 	{
-		value: 'Prefer not to say',
 		testId: 'prefer',
 		formatMessageId: 'aboutyou.answer-prefer-not-to-say'
 	}
--- src/data/organizationTypeFormOptions.js
+++ src/data/organizationTypeFormOptions.js
@@ -1,21 +1,17 @@
 export const organizationTypesLawyer = [
 	{
-		value: 'Corporate law firm',
 		testId: 'corp',
 		formatMessageId: 'account.sign-up-orgType-answer-corp'
 	},
 	{
-		value: 'Law school student clinic',
 		testId: 'law',
 		formatMessageId: 'account.sign-up-orgType-answer-law'
 	},
 	{
-		value: 'Legal nonprofit',
 		testId: 'legal-non-profit',
 		formatMessageId: 'account.sign-up-orgType-answer-legal-non-profit'
 	},
 	{
-		value: 'Other (specify)',
 		testId: 'other',
 		formatMessageId: 'aboutyou.answer-other'
 	}
@@ -23,37 +19,30 @@ export const organizationTypesLawyer = [
 
 export const organizationTypesProvider = [
 	{
-		value: 'Healthcare Provider',
 		testId: 'healthcare',
 		formatMessageId: 'account.sign-up-orgType-answer-healthcare'
 	},
 	{
-		value: 'Government Agency',
 		testId: 'government',
 		formatMessageId: 'account.sign-up-orgType-answer-government'
 	},
 	{
-		value: 'Nonprofit',
 		testId: 'non-profit',
 		formatMessageId: 'account.sign-up-orgType-answer-non-profit'
 	},
 	{
-		value: 'Grassroots Direct Services Org',
 		testId: 'grassroots',
 		formatMessageId: 'account.sign-up-orgType-answer-grassroots'
 	},
 	{
-		value: 'Student Club',
 		testId: 'student',
 		formatMessageId: 'account.sign-up-orgType-answer-student'
 	},
 	{
-		value: "N/A (I'm looking for safe resources for friends or family)",
 		testId: 'na',
 		formatMessageId: 'account.sign-up-orgType-answer-na'
 	},
 	{
-		value: 'Other (specify)',
 		testId: 'other',
 		formatMessageId: 'aboutyou.answer-other'
 	}
