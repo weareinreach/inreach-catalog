7df6fe50176d4fba316209dc28e4b214c36c40e3
dlg <>
2022-02-07 20:50:57 -0800

    translations and test updates for Forgot Password form

--- cypress/support/reusable_tests/forgot_password.js
+++ cypress/support/reusable_tests/forgot_password.js
@@ -1,61 +1,129 @@
 Cypress.Commands.add('testForgotPasswordElements',(viewport)=>{
-cy.viewport(viewport);
-//Sign in Button
-if(viewport === Cypress.env('mobile')){
-    cy.getElementByTestId('mobile-nav-button-account').then($element =>{
-        cy.wrap($element).click();
-    });
-}else{
-    cy.getElementByTestId('nav-account-sign-in').then($element => {
-        cy.wrap($element).click({force:true});
-    });
-}
-cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element=>{
-    cy.wrap($element).click();
-if(viewport !== Cypress.env('mobile')){
-    cy.getElementByTestId('dialog-container-title').then($element=>{
-        expect($element).to.be.visible;
-        expect($element).contain("Reset Password");
-    });
-}
-    cy.getElementByTestId('forgot-password-body').then($element=>{
-        expect($element).to.be.visible;
-        expect($element).contain("We'll send you an email to reset your password.");
-    });
-    cy.getElementByTestId('forgot-password-email').then($element=>{
-        expect($element).to.be.visible;
-        expect($element.children()).contain('Email');
-    });
-    cy.getElementByTestId('forgot-password-send-button').then($element=>{
-        expect($element).to.be.visible;
-        expect($element).to.have.attr('type','submit');
-    });
-    cy.getElementByTestId('forgot-password-back-button').then($element=>{
-        expect($element).to.be.visible;
-        expect($element.children()).contain('Back');
-        cy.wrap($element).click();
-    });
-});
-
-});
-
-
-Cypress.Commands.add('testForgotPasswordAction',(viewport,email)=>{
     cy.viewport(viewport);
-//Sign in Button
-if(viewport === Cypress.env('mobile')){
-    cy.getElementByTestId('mobile-nav-button-account').then($element =>{
+    //Sign in Button
+    if(viewport === Cypress.env('mobile')){
+        cy.getElementByTestId('mobile-nav-button-account').then($element =>{
+            cy.wrap($element).click();
+        });
+    }else{
+        cy.getElementByTestId('nav-account-sign-in').then($element => {
+            cy.wrap($element).click({force:true});
+        });
+    }
+    cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element=>{
         cy.wrap($element).click();
+    if(viewport !== Cypress.env('mobile')){
+        cy.getElementByTestId('dialog-container-title').then($element=>{
+            expect($element).to.be.visible;
+            expect($element).contain("Reset Your Password");
+        });
+    }
+        cy.getElementByTestId('forgot-password-body').then($element=>{
+            expect($element).to.be.visible;
+            expect($element).contain("Enter your email address and we will send you an email to reset your password.");
+        });
+        cy.getElementByTestId('forgot-password-email').then($element=>{
+            expect($element).to.be.visible;
+            expect($element.children()).contain('Email');
+        });
+        cy.getElementByTestId('forgot-password-send-button').then($element=>{
+            expect($element).to.be.visible;
+            expect($element).to.have.attr('type','submit');
+        });
+        cy.getElementByTestId('forgot-password-back-button').then($element=>{
+            expect($element).to.be.visible;
+            expect($element).contain('Back');
+            cy.wrap($element).click();
+        });
     });
-}else{
-    cy.getElementByTestId('nav-account-sign-in').then($element => {
-        cy.wrap($element).click({force:true});
-    });
-}
-cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element=>{
-    cy.wrap($element).click();
-    cy.getElementByTestId('forgot-password-email').type(email);
-    cy.getElementByTestId('forgot-password-send-button').click();
-
 });
+
+Cypress.Commands.add('testForgotPasswordActionGoodEmail',(viewport,email)=>{
+    cy.viewport(viewport);
+    //Sign in Button
+    if(viewport === Cypress.env('mobile')){
+        cy.getElementByTestId('mobile-nav-button-account').then($element =>{
+            cy.wrap($element).click();
+        });
+    }else{
+        cy.getElementByTestId('nav-account-sign-in').then($element => {
+            cy.wrap($element).click({force:true});
+        });
+    }    
+
+    cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element=>{
+        cy.wrap($element).click();
+        cy.getElementByTestId('forgot-password-email').type(email);
+        cy.getElementByTestId('forgot-password-send-button').click();
+
+         //look for success message
+        cy.getElementByTestId('snackbar-message').should('be.visible').then($element=>{
+            expect($element).to.be.visible;
+            expect($element).contain("Your password has been reset successfully! Please check your email.");
+            cy.getElementByTestId('snackbar-close-button').should('be.visible');
+            cy.getElementByTestId('snackbar-close-button').click();
+            cy.getElementByTestId('snackbar-message').should('not.exist');
+        });
+    });
+});
+
+Cypress.Commands.add('testForgotPasswordActionBadEmail',(viewport,email)=>{
+    cy.viewport(viewport);
+    //Sign in Button
+    if(viewport === Cypress.env('mobile')){
+        cy.getElementByTestId('mobile-nav-button-account').then($element =>{
+            cy.wrap($element).click();
+        });
+    }else{
+        cy.getElementByTestId('nav-account-sign-in').then($element => {
+            cy.wrap($element).click({force:true});
+        });
+    }
+    cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element=>{
+        cy.wrap($element).click();
+        cy.getElementByTestId('forgot-password-email').type(email);
+        cy.getElementByTestId('forgot-password-send-button').click();
+
+         //look for error message
+        cy.getElementByTestId('snackbar-message').should('be.visible').then($element=>{
+            expect($element).to.be.visible;
+            expect($element).contain("Oops! Please check that you entered the correct email address.");
+            cy.getElementByTestId('snackbar-close-button').should('be.visible');
+            cy.getElementByTestId('snackbar-close-button').click();
+            cy.getElementByTestId('snackbar-message').should('not.exist');
+        });
+    });
+});
+
+Cypress.Commands.add('testGenericError',(viewport, email)=>{
+ cy.viewport(viewport);
+    //Sign in Button
+    if(viewport === Cypress.env('mobile')){
+        cy.getElementByTestId('mobile-nav-button-account').then($element =>{
+            cy.wrap($element).click();
+        });
+    }else{
+        cy.getElementByTestId('nav-account-sign-in').then($element => {
+            cy.wrap($element).click({force:true});
+        });
+    }
+    cy.getElementByTestId('log-in-dialog-container-forgot-password').then($element=>{
+        cy.wrap($element).click();
+        cy.getElementByTestId('forgot-password-email').type(email);
+        
+        //intercept and force generic error
+        cy.intercept('POST', '/v1/users/forgotPassword',{
+            forceNetworkError: true 
+        });
+        cy.getElementByTestId('forgot-password-send-button').click();
+
+        //look for error message
+        cy.getElementByTestId('snackbar-message').should('be.visible').then($element=>{
+            expect($element).to.be.visible;
+            expect($element).contain("Oops! Something went wrong.");
+                cy.getElementByTestId('snackbar-close-button').should('be.visible');
+                cy.getElementByTestId('snackbar-close-button').click();
+                cy.getElementByTestId('snackbar-message').should('not.exist');
+            });
+        });
 });
--- src/components/ForgotDialog.js
+++ src/components/ForgotDialog.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {FormattedMessage} from 'react-intl';
 
 import DialogTitle from './DialogTitle';
 import ForgotFormContainer from './ForgotFormContainer';
 
 const ForgotDialog = ({
-  handleMessageNew,
-  handleRequestClose,
-  handleRequestOpen,
+	handleMessageNew,
+	handleRequestClose,
+	handleRequestOpen
 }) => (
-  <div>
-    <DialogTitle>Reset Password</DialogTitle>
-    <ForgotFormContainer
-      handleMessageNew={handleMessageNew}
-      handleRequestClose={handleRequestClose}
-      handleRequestOpen={handleRequestOpen}
-    />
-  </div>
+	<div>
+		<DialogTitle>
+			<FormattedMessage id="account.reset-password" />
+		</DialogTitle>
+		<ForgotFormContainer
+			handleMessageNew={handleMessageNew}
+			handleRequestClose={handleRequestClose}
+			handleRequestOpen={handleRequestOpen}
+		/>
+	</div>
 );
 
 ForgotDialog.propTypes = {
-  handleMessageNew: PropTypes.func.isRequired,
-  handleRequestClose: PropTypes.func.isRequired,
-  handleRequestOpen: PropTypes.func.isRequired,
+	handleMessageNew: PropTypes.func.isRequired,
+	handleRequestClose: PropTypes.func.isRequired,
+	handleRequestOpen: PropTypes.func.isRequired
 };
 
 export default ForgotDialog;
