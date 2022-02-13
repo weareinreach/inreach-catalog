e4d44e4b971b47f3013dee5888114ec54615b276
dlg <>
2022-02-12 19:58:03 -0800

    localization changes for Privacy components

--- src/components/PrivacyDialog.js
+++ src/components/PrivacyDialog.js
@@ -1,20 +1,25 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {FormattedMessage} from 'react-intl';
 
 import DialogButton from './DialogButton';
 import DialogTitle from './DialogTitle';
 import PrivacyText from './PrivacyText';
 
 const PrivacyDialog = ({handleRequestClose}) => (
-  <div>
-    <DialogTitle>AsylumConnect User Privacy Statement</DialogTitle>
-    <PrivacyText />
-    <DialogButton handleRequestClose={handleRequestClose}>OK</DialogButton>
-  </div>
+	<div>
+		<DialogTitle>
+			<FormattedMessage id="legal.privacy-statement" />
+		</DialogTitle>
+		<PrivacyText />
+		<DialogButton handleRequestClose={handleRequestClose}>
+			<FormattedMessage id="action.ok" />
+		</DialogButton>
+	</div>
 );
 
 PrivacyDialog.propTypes = {
-  handleRequestClose: PropTypes.func.isRequired,
+	handleRequestClose: PropTypes.func.isRequired
 };
 
 export default PrivacyDialog;
