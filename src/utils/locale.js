ff68675f714c68d2a127f84010dfede8035f911d
dlg <>
2022-02-14 17:11:52 -0800

    comments for localization changes

--- src/utils/locale.js
+++ src/utils/locale.js
@@ -48,19 +48,47 @@ const enMX = {
 	'-98.585522': '-102.552784'
 };
 
+//will need this once the catalog is fully translated to Spanish (also need to translate this section)
+/*const esMX = {
+	'Start typing county, city or state in the US…':
+		'Start typing city or state in Mexico…',
+	'Download Legal Guides on LGBTQ Asylum in the U.S.':
+		'Download Legal Guides on LGBTQ Asylum in Mexico',
+	'English classes': 'Language classes',
+	'LGBTQ centers': 'LGBTQ centres',
+	'Cultural centers': 'Cultural centres',
+	'Drop-in centers for LGBTQ youth': 'Drop-in centres for LGBTQ youth',
+	'Welcome to the United States AsylumConnect Catalog!':
+		'Welcome to the Mexico AsylumConnect Catalog!',
+	'Show me national organizations who can help anyone located in the United States':
+		'Show me national organizations who can help anyone located in Mexico',
+	'Physical evaluations for asylum claim':
+		'Physical evaluations for refugee claim',
+	'Asylum application': 'Refugee claim',
+	'Psychological evaluations for asylum claim':
+		'Psychological evaluations for refugee claim',
+	'Career counseling': 'Career counselling',
+	'Private therapy and counseling': 'Private therapy and counselling',
+	39.8333333: '23.634501',
+	'-98.585522': '-102.552784'
+}*/
+
 export const fetchLocale = (locale) => {
 	switch (locale) {
 		case 'en_CA':
 			return enCA;
 		case 'en_MX':
 			return enMX;
+		//will need this once the catalog is fully translated to Spanish
+		/* case 'es_MX':
+			return esMX; */
 		case 'en_US':
 		default:
 			return enUS;
 	}
 };
 
-export const validLocales = ['en_US', 'en_CA', 'en_MX'];
+export const validLocales = ['en_US', 'en_CA', 'en_MX', 'es_MX'];
 
 export const resetLocale = () => {
 	window.localStorage.setItem('locale', 'en_US');
