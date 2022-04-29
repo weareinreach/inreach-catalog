23d881650699fcdcc49749d638d1ed6eac75c8f1
dlg <>
2022-04-29 01:19:44 -0700

    update top banner: AsylumConnect to InReach, remove disclaimer

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -3,95 +3,38 @@ import {FormattedMessage} from 'react-intl';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import {withStyles} from '@material-ui/core/styles';
+import {InformationIcon24} from './icons';
 
 const styles = (theme) => ({
 	announcement: {
-		backgroundColor: theme.palette.secondary[500],
-		padding: '2rem 0',
+		backgroundColor: theme.palette.banner[500],
+		padding: '1rem 0',
 		textAlign: 'center'
 	},
-	pointer: {
-		cursor: 'pointer',
-		display: 'inline-block',
-		position: 'relative'
-	},
-	pointerText: {
-		textShadow:
-			'2px 0 ' +
-			theme.palette.secondary[500] +
-			', -2px 0 ' +
-			theme.palette.secondary[500]
-	},
-	underline: {
-		position: 'absolute',
-		width: '100%',
-		display: 'block',
-		margin: '0',
-		border: 'none',
-		height: '1px',
-		background: 'white',
-		bottom: '.2em'
-	},
 	textContent: {
-		color: theme.palette.common.darkWhite,
+		color: theme.palette.common.black,
 		maxWidth: theme.maxColumnWidth,
-		margin: '0 auto'
+		margin: '0 16px',
+		fontSize: '24px',
+		textDecorationLine: 'underline'
 	}
 });
 
-const Announcement = ({classes, handleRequestOpen}) => (
+const Announcement = ({classes}) => (
 	<div className={classes.announcement + ' hide--on-print'}>
+		<InformationIcon24 />
 		<Typography
-			variant="body1"
+			variant="bannerText"
 			className={classes.textContent}
 			data-test-id="announcement-header"
 		>
-			<FormattedMessage id="legal.user-contact-provider-risk" />
-			<br />
-			<span>
-				<FormattedMessage id="legal.read-disclaimer-prompt-pt1" />{' '}
-			</span>
-			<strong
-				className={classes.pointer}
-				onClick={() => handleRequestOpen('disclaimer')}
-			>
-				<i className={classes.underline} />
-				<span
-					className={classes.pointerText}
-					data-test-id="announcement-disclaimer-button"
-				>
-					{' '}
-					<FormattedMessage id="legal.read-disclaimer-prompt-pt2" />{' '}
-				</span>
-			</strong>
-			<span>
-				{' '}
-				<FormattedMessage id="legal.and" />{' '}
-			</span>
-			<strong
-				className={classes.pointer}
-				onClick={() => handleRequestOpen('privacy')}
-			>
-				<i className={classes.underline} />
-				<span
-					className={classes.pointerText}
-					data-test-id="announcement-privacy-button"
-				>
-					{' '}
-					<FormattedMessage id="legal.read-disclaimer-prompt-pt3" />{' '}
-				</span>
-			</strong>
-			<span>
-				{' '}
-				<FormattedMessage id="legal.read-disclaimer-prompt-pt4" />
-			</span>
+			<FormattedMessage id="announcement-brand" />
 		</Typography>
 	</div>
 );
 
 Announcement.propTypes = {
-	classes: PropTypes.object.isRequired,
-	handleRequestOpen: PropTypes.func.isRequired
+	classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Announcement);
