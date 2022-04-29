3752750de6399a7cd7ed7c6cfa7843570289fa64
dlg <>
2022-04-29 15:07:24 -0700

    new bottom banner, update colors for MX crime data

---
+++ src/components/BottomBanner.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = (theme) => ({
+	bgLightGrey: {backgroundColor: theme.palette.common.lightGrey},
+	textCenter: {textAlign: 'center'},
+	content: {
+		maxWidth: '80%',
+		margin: '0 10%'
+	},
+	paddingVertical: {padding: '1.5rem 0'}
+});
+
+const BottomBanner = ({classes}) => {
+	const {bgLightGrey, textCenter, content, paddingVertical} = classes;
+
+	return (
+		<div className={classNames(textCenter, bgLightGrey)}>
+			<div className={classNames(content)}>
+				<Typography
+					variant="subtitle2"
+					className={classes.paddingVertical}
+					data-test-id="announcement-header"
+				>
+					<FormattedMessage id="app.banner" />
+				</Typography>
+				<Typography
+					variant="subtitle1"
+					className={classes.paddingVertical}
+					data-test-id="announcement-header"
+				>
+					<FormattedMessage id="app.welcome-4" />
+				</Typography>
+			</div>
+		</div>
+	);
+};
+
+export default withStyles(styles)(BottomBanner);
