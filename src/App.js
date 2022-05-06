1af5481d3ab26d8ef0ea61c5ae97243121c7d231
dlg <>
2022-05-06 15:57:13 -0700

    updating logo, move old logos to temp 'old' folder

--- src/App.js
+++ src/App.js
@@ -27,11 +27,11 @@ import DeleteListMobile from './components/DeleteListMobile';
 import ThankYouMobile from './components/ThankYouMobile';
 
 import withWidth from './components/withWidth';
-import LogoImg from './images/logo.png';
+import LogoImg from './images/logoInReach.png';
 import LogoImgMobile from './images/logo-mobile@3x.png';
-import LogoImgCA from './images/logo-ca@2x.png';
+import LogoImgCA from './images/logoInReach.png';
 import LogoImgMXMobile from './images/logo-mx@2x.png';
-import LogoImgMX from './images/logo-mx.png';
+import LogoImgMX from './images/logoInReach.png';
 import {breakpoints} from './theme';
 import {fetchUser} from './utils/api';
 import {
