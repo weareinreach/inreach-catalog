7e69423d31c1f49f1f4c0e588e7ec6660c355b41
dlg <>
2022-06-19 19:46:05 -0700

    adding defaultMessage and description to FormattedMessage - AboutYouAge component

--- src/components/AboutYouAge.js
+++ src/components/AboutYouAge.js
@@ -150,10 +150,18 @@ const AboutYouAge = (props) => {
 	return (
 		<>
 			<DialogTitle>
-				<FormattedMessage id="account.signup-about-you" />
+				<FormattedMessage
+					id="account.signup-about-you"
+					defaultMessage="About You"
+					description="Title for the About You sign up dialog"
+				/>
 			</DialogTitle>
 			<DialogSubTitle className={classes.sideMargin}>
-				<FormattedMessage id="account.signup-about-you-subtitle" />
+				<FormattedMessage
+					id="account.signup-about-you-subtitle"
+					defaultMessage="Help us improve your experience by telling us more about yourself"
+					description="Sub-title for the About You sign up dialog"
+				/>
 			</DialogSubTitle>
 			<div className={classes.greyLine} />
 			<form
@@ -164,7 +172,11 @@ const AboutYouAge = (props) => {
 				data-test-id="about-you-age-form"
 			>
 				<Typography className={classes.formQuestion} variant="h3">
-					<FormattedMessage id="aboutyou.age" />
+					<FormattedMessage
+						id="aboutyou.age"
+						defaultMessage="How old are you?"
+						description="Question asking your age"
+					/>
 				</Typography>
 				<RadioGroup name="age" onChange={handleChange} required={true}>
 					<Grid container spacing={0} className={classes.gridTxtAlign}>
@@ -186,7 +198,11 @@ const AboutYouAge = (props) => {
 					variant="primary"
 					className={classes.nextBtn}
 				>
-					<FormattedMessage id="action.submit" />
+					<FormattedMessage
+						id="action.submit"
+						defaultMessage="Submit"
+						description="This is the form submit button"
+					/>
 				</AsylumConnectButton>
 			</form>
 		</>
