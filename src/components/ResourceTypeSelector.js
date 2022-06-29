3135c0a96db8e340aeb111d1791149fd9716905b
dlg <>
2022-06-29 12:30:10 -0700

    adding Internationalization to Tags and ResourceTypeSelector

--- src/components/ResourceTypeSelector.js
+++ src/components/ResourceTypeSelector.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FormattedMessage} from 'react-intl';
+import {FormattedMessage, useIntl} from 'react-intl';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -198,6 +198,7 @@ const FilterCollectionMobile = (props) => {
 	const categoryValue = hasChildren
 		? children?.map((item) => `${props.value}.${item.value}`).join(',')
 		: props.value;
+	const intl = useIntl();
 
 	return (
 		<div>
@@ -220,7 +221,9 @@ const FilterCollectionMobile = (props) => {
 					</span>
 				) : null}
 				<ACBadge type={type} width="45px" height="45px" useIcon={true} />
-				<span className={classes.sectionTitle}>{category}</span>
+				<span className={classes.sectionTitle}>
+					{intl.formatMessage({id: category})}
+				</span>
 				{hasChildren ? <ExpandMoreIcon className={classes.arrowIcon} /> : null}
 			</Typography>
 			{hasChildren && clickedCategory === index ? (
@@ -231,7 +234,7 @@ const FilterCollectionMobile = (props) => {
 						return (
 							<Grid item key={i} xs={12} sm={6} md={4}>
 								<AsylumConnectCheckbox
-									label={filter.title}
+									label={intl.formatMessage({id: filter.title})}
 									value={itemValue}
 									onChange={onChange}
 									disabled={selectedResourceTypes.indexOf(categoryValue) >= 0}
@@ -279,6 +282,7 @@ const FilterCollection = (props) => {
 	} else if (hoveredCategory === index) {
 		backgroundColor = '#D3DCEC';
 	}
+	const intl = useIntl();
 
 	return (
 		<div
@@ -301,7 +305,9 @@ const FilterCollection = (props) => {
 					</span>
 				) : null}
 				<ACBadge type={type} width="45px" height="45px" useIcon={true} />
-				<span className={classes.sectionTitle}>{category}</span>
+				<span className={classes.sectionTitle}>
+					{intl.formatMessage({id: category})}
+				</span>
 				{hasChildren ? (
 					<ArrowForwardIosIcon className={classes.arrowIcon} />
 				) : null}
@@ -333,6 +339,7 @@ const FilterSubCollection = (props) => {
 	const categoryValue = hasChildren
 		? children?.map((item) => `${props.value}.${item.title}`).join(',')
 		: props.value;
+	const intl = useIntl();
 
 	return (
 		<div>
@@ -351,16 +358,17 @@ const FilterSubCollection = (props) => {
 												<Tooltip
 													data-test-id="filter-info-tooltip"
 													classes={{tooltipPlacementTop: 'badge-tooltipTop'}}
-													title={filter.info}
+													title={intl.formatMessage({id: filter.info})}
 													placement="top"
 												>
 													<div data-test-id="filter-title">
-														{filter.title} <InformationIcon />
+														{intl.formatMessage({id: filter.title})}{' '}
+														<InformationIcon />
 													</div>
 												</Tooltip>
 											</>
 										) : (
-											filter.title
+											intl.formatMessage({id: filter.title})
 										)
 									}
 									interactive
