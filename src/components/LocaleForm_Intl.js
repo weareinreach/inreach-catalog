36e3f521b777aa5b3c9517448c2e069fa609d50a
trigal <10123216+trigal2012@users.noreply.github.com>
2022-09-14 09:02:20 -0700

    syncing intl and notintl files with dev

--- src/components/LocaleForm_Intl.js
+++ src/components/LocaleForm_Intl.js
@@ -3,7 +3,6 @@ import {FormattedMessage} from 'react-intl';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import {withStyles} from '@material-ui/core/styles';
-
 import AsylumConnectButton from './AsylumConnectButton';
 import Language from './Language';
 import LocaleSelector from './LocaleSelector';
