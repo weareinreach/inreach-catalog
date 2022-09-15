2416ccdadb2c492bc656995b9ba5f138710f5624
trigal <10123216+trigal2012@users.noreply.github.com>
2022-09-15 10:50:18 -0700

    env test 2

--- src/config.js
+++ src/config.js
@@ -14,10 +14,8 @@ const prod = {
 	apiDomain: 'https://inreach-api.herokuapp.com'
 };
 
-const env = process.env.REACT_APP_APP_ENV;
-const useIntl = process.env.USE_INTL;
 console.log(process.env);
-console.log(useIntl);
+const env = process.env.REACT_APP_APP_ENV;
 
 export default {
 	...base,
